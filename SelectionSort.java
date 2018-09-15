7aeb2fc864231fe5518ee6275fcb82d9c3621810
Steph <42854764+stephannapolis@users.noreply.github.com>
2018-09-14 20:49:37 -0400

    fill in selectionsort
    create code for selection sort in both recursive and iterative
    add check if sorted array is sorted

--- SelectionSort.java
+++ SelectionSort.java
@@ -6,26 +6,79 @@
 
 class SelectionSort implements SortInterface {
    
+   private int count;
+   private long time;
+   
    public SelectionSort() {
+      count = 0;
+      time = 0;
    } //end default constructor
    
    @Override
    public void recursiveSort(int[] list) throws UnsortedException {
+      count = 0;
+      long start = System.nanoTime();
+      recursiveSortInner(list, 0);
+      long time = System.nanoTime() - start;
+      
+      checkSorted(list);
    } //end recursiveSort()
+   
+   private void recursiveSortInner(int[] list, int j){
+      if (j >= list.length - 1) return;
+      
+      int min = j;
+      for (int i = j + 1; i < list.length; i++) {
+         count ++; //critical operation, comparing each item to min
+         if (list[i] < list [min]) min = i;
+      }
+      int temp = list[j];
+      list[j] = list[min];
+      list[min] = temp;
+      
+      recursiveSortInner(list, j + 1);
+
+   }
    
    @Override
    public void iterativeSort(int[] list) throws UnsortedException {
+      count = 0;
+      long start = System.nanoTime();
+      int n = list.length;
+      
+      for (int j=0; j < n-1; j++) {
+         int min = j;
+         
+         for (int i = j+1; i < n; i++){
+            count++; //critical operation, comparing each item to the min
+            if (list[i] < list[min]) {
+               min = i;
+            }
+         }
+         int temp = list[min];
+         list[min] = list[j];
+         list[j] = temp;
+      }
+      time = System.nanoTime() - start;
+      
+      checkSorted(list);
    } //end iterativeSort()
    
    @Override
    public int getCount() {
-      return 1;
+      return count;
    } //end getCount()
    
    @Override
    public long getTime() {
-      return 1;
+      return time;
    } //end getTime()
+   
+   private void checkSorted(int[] list) throws UnsortedException {
+      for (int i = 0; i < list.length - 1; i++){
+         if (list[i] > list[i+1]) throw new UnsortedException("Array not sorted correctly.");
+      }
+   }
    
    
 }//end class SelectionSort
