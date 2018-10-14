ba56b44cb0eca7a57dadbec026cac0d657f3762d
Steph <42854764+stephannapolis@users.noreply.github.com>
2018-10-14 17:54:58 -0400

    update array sizes
    increase array sizes and spread evenly for better data visual

--- SortMain.java
+++ SortMain.java
@@ -16,7 +16,7 @@ public class SortMain{
    
    //main()
    public static void main (String [] args) {
-      int[] sizes = {10,100,250,500,1000,2500,5000,7500,10000,25000};
+      int[] sizes = {2500,5000,7500,10000,12500,15000,17500,20000,22500,25000};
       
       BenchmarkSorts sorter = new BenchmarkSorts(sizes);
       sorter.runSorts();
